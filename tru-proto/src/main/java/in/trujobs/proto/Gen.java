3717c881c4de1021528425e38e204d8386098552
zero <ethereal.sandeep@gmail.com>
2016-07-29 00:03:34 +0530

    bash file for compile added + homeLocReq added

--- tru-proto/src/main/java/in/trujobs/proto/Gen.java
+++ tru-proto/src/main/java/in/trujobs/proto/Gen.java
@@ -63,6 +63,16 @@ public final class Gen {
   static
     com.google.protobuf.GeneratedMessage.FieldAccessorTable
       internal_static_in_trujobs_proto_JobPost_fieldAccessorTable;
+  static com.google.protobuf.Descriptors.Descriptor
+    internal_static_in_trujobs_proto_HomeLocalityRequest_descriptor;
+  static
+    com.google.protobuf.GeneratedMessage.FieldAccessorTable
+      internal_static_in_trujobs_proto_HomeLocalityRequest_fieldAccessorTable;
+  static com.google.protobuf.Descriptors.Descriptor
+    internal_static_in_trujobs_proto_HomeLocalityResponse_descriptor;
+  static
+    com.google.protobuf.GeneratedMessage.FieldAccessorTable
+      internal_static_in_trujobs_proto_HomeLocalityResponse_fieldAccessorTable;
 
   public static com.google.protobuf.Descriptors.FileDescriptor
       getDescriptor() {
@@ -103,7 +113,14 @@ public final class Gen {
       "ost\"\202\001\n\007JobPost\022\021\n\tjobPostId\030\001 \001(\003\022\024\n\014jo" +
       "bPostTitle\030\002 \001(\t\022\032\n\022jobPostCompanyName\030\003",
       " \001(\t\022\030\n\020jobPostMinSalary\030\004 \001(\003\022\030\n\020jobPos" +
-      "tMaxSalary\030\005 \001(\003B\002P\001b\006proto3"
+      "tMaxSalary\030\005 \001(\003\"~\n\023HomeLocalityRequest\022" +
+      "\013\n\003lat\030\001 \001(\001\022\013\n\003lng\030\002 \001(\001\022\014\n\004name\030\003 \001(\t\022" +
+      "\017\n\007address\030\004 \001(\t\022\030\n\020candidate_mobile\030\005 \001" +
+      "(\t\022\024\n\014candidate_id\030\006 \001(\003\"\232\001\n\024HomeLocalit" +
+      "yResponse\022=\n\006status\030\001 \001(\0162-.in.trujobs.p" +
+      "roto.HomeLocalityResponse.Status\"C\n\006Stat" +
+      "us\022\013\n\007UNKNOWN\020\000\022\013\n\007SUCCESS\020\001\022\013\n\007FAILURE\020" +
+      "\002\022\022\n\016USER_NOT_FOUND\020\003B\002P\001b\006proto3"
     };
     com.google.protobuf.Descriptors.FileDescriptor.InternalDescriptorAssigner assigner =
         new com.google.protobuf.Descriptors.FileDescriptor.    InternalDescriptorAssigner() {
@@ -183,6 +200,18 @@ public final class Gen {
       com.google.protobuf.GeneratedMessage.FieldAccessorTable(
         internal_static_in_trujobs_proto_JobPost_descriptor,
         new java.lang.String[] { "JobPostId", "JobPostTitle", "JobPostCompanyName", "JobPostMinSalary", "JobPostMaxSalary", });
+    internal_static_in_trujobs_proto_HomeLocalityRequest_descriptor =
+      getDescriptor().getMessageTypes().get(11);
+    internal_static_in_trujobs_proto_HomeLocalityRequest_fieldAccessorTable = new
+      com.google.protobuf.GeneratedMessage.FieldAccessorTable(
+        internal_static_in_trujobs_proto_HomeLocalityRequest_descriptor,
+        new java.lang.String[] { "Lat", "Lng", "Name", "Address", "CandidateMobile", "CandidateId", });
+    internal_static_in_trujobs_proto_HomeLocalityResponse_descriptor =
+      getDescriptor().getMessageTypes().get(12);
+    internal_static_in_trujobs_proto_HomeLocalityResponse_fieldAccessorTable = new
+      com.google.protobuf.GeneratedMessage.FieldAccessorTable(
+        internal_static_in_trujobs_proto_HomeLocalityResponse_descriptor,
+        new java.lang.String[] { "Status", });
   }
 
   // @@protoc_insertion_point(outer_class_scope)
